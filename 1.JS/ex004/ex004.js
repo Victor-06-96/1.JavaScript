d53305c1d710f846fcb1e9be40d3ec26c53106af
Victor-06-96 <victor.castro96@outlook.com>
2023-03-02 15:56:50 -0300

    Aprendendo JavaScript

---
+++ 1.JS/ex004/ex004.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+console.log(`Dirija sempre usando cinto de segurança!`)
+if (vel>60) {
+    console.log(`Você ultrapassou a velocidad permitida. MULTADO!`)
+}
