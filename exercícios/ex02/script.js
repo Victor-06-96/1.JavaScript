55868918e8de2a728d1f5eef8f621bc9c23f79d2
Victor-06-96 <victor.castro96@outlook.com>
2023-03-14 12:44:48 -0300

    aprendendo JS

--- exercícios/ex02/script.js
+++ exercícios/ex02/script.js
@@ -8,42 +8,58 @@ function verificar() {
     } else {
         var sexo = window.document.getElementsByName(`sexo`)
         var idade = hoje - Number(nasc.value)
+        var img = document.createElement(`img`)
+        img.setAttribute(`ìd`, `foto`) //criando uma area para imagem sem precisar criar em HTML
         var genero = ``
         if (sexo[0].checked) {
             if (idade < 10) {
                 genero = `menino`
+                img.setAttribute(`src`, `imagens/menino.jpg`)
                 //criança
             } else if (idade < 20) {
                 genero = `garoto`
+                img.setAttribute(`src`, `imagens/garoto.jpg`)
                 //adolescente
             } else if (idade < 50) {
                 genero = `Homem`
+                img.setAttribute(`src`, `imagens/homem.jpg`)
                 //adulto
             } else if (idade < 75) {
                 genero = `Velho`
+                img.setAttribute(`src`, `imagens/velho.jpg`)
                 //velho
             } else {
                 genero = `Idoso`
+                img.setAttribute(`src`, `imagens/idoso.jpg`)
                 //idoso
             }
         } else if (sexo[1].checked) {
             if (idade < 10) {
                 genero = `menina`
+                img.setAttribute(`src`, `imagens/menina.jpg`)
                 //criança
             } else if (idade < 20) {
                 genero = `garota`
+                img.setAttribute(`src`, `imagens/garota.jpg`)
                 //adolescente
             } else if (idade < 50) {
                 genero = `Mulher`
+                img.setAttribute(`src`, `imagens/mulher.jpg`)
                 //adulto
             } else if (idade < 75) {
                 genero = `Velha`
+                img.setAttribute(`src`, `imagens/velha.jpg`)
                 //velho
             } else {
                 genero = `Idosa`
+                img.setAttribute(`src`, `imagens/idosa.jpg`)
                 //idoso
             }
         }
         resp.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        resp.appendChild(img)
+        resp.style.textAlign = `center`
+        img.style.width = `300px`
+        img.style.borderRadius = `100%`
     }
 }
