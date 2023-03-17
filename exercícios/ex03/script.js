483d53c15c05720992ef0ce865bdc65bd2e53d06
Victor-06-96 <victor.castro96@outlook.com>
2023-03-17 15:55:52 -0300

    Aprendendo JS

--- exercícios/ex03/script.js
+++ exercícios/ex03/script.js
@@ -1,15 +1,25 @@
 function contar() {
    var ini = parseInt(window.document.querySelector(`#inicio`).value)
-   var fim = parseInt(window.document.querySelector(`#fim`).value) + 1 
+   var fim = parseInt(window.document.querySelector(`#fim`).value)
    var passo = parseInt(window.document.querySelector(`#passo`).value) 
    var comando = window.document.querySelector(`#comando`)
    if (!inicio || !fim || !passo) {
-    window.alert(`[ERRO] Digite um Número nos campos abaixo`)
-   } else {
+    comando.innerHTML = `Impossivel Contar`
+    comando.style.color = `red`
+   } else if(ini < fim) { 
     comando.innerHTML = `Contando: </br>`
     for(var i = ini ; i <= fim ; i += passo) {
-        comando.innerHTML += ` > ${i}`
-    }
+        comando.innerHTML += ` ${i} \u{1F449}`
+        }
+        comando.innerHTML += `\u{1F3C1}`
+        comando.style.color = `black`
+   } else {
+    comando.innerHTML = `Contando: </br>`
+    for(var i = ini ; i >= fim ; i -= passo) {
+        comando.innerHTML += ` ${i} \u{1F449}`
+        }
+        comando.innerHTML += `\u{1F3C1}`
+        comando.style.color = `black`
    }
 }
 
